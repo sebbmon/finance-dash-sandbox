b4b0471be8a7812e94ec05d1c94c40660b7e479f
Sebastian <sebekmo01@gmail.com>
2026-06-07 21:34:52 +0200

    google analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
 });
 
+const googleTagManagerId = "GTM-W3WFL7TJ";
+
 export const metadata: Metadata = {
   title: "FinanceDash - Personal Budget",
   description: "A simple personal budget dashboard",
@@ -29,6 +31,22 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className="font-sans bg-zinc-100 dark:bg-zinc-950 text-zinc-950 dark:text-zinc-50 antialiased min-h-screen">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${googleTagManagerId}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${googleTagManagerId}');`}
+        </Script>
         <AuthProvider>
           <AuthGate>
             <AppLayout>
